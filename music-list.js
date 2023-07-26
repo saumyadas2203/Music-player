9239db4724f00988f25fe7ec13b3e0333cd53ec3
saumyadas2203 <140593143+saumyadas2203@users.noreply.github.com>
2023-07-26 13:22:04 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Sunn raha hai",
+        artist: "Shreya Ghosal",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Lut Gaye",
+        artist: "Jubin Nautiyal",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Khuda Jaane",
+        artist: "KK",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Saami Saami",
+        artist: "Sunidhi Chauhan",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Kesariya",
+        artist: "Arjit Singh",
+        img: "music-5",
+        src: "music-5"
+    },
+];   
